0a617bce56813cae20b08d353e04923778ced31c
RafaelAmaral <rafaelamaralrodri@gmail.com>
2026-03-13 21:01:52 -0300

    Setup inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+     <div>
+      <h1>Funil de Vendas</h1>
+      <p>Aplicação para montar funis de tráfego pago</p>
+    </div>
+  )
+}
+
+export default App
